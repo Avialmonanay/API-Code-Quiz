123552ee8c2698cf1029e49035b4de9388c5549d
Avialmonanay <108016215+Avialmonanay@users.noreply.github.com>
2022-08-07 21:46:15 -0600

    event listeners added, splice added to keep questions from repeating

--- Game-Page-Assets/Java/Script.js
+++ Game-Page-Assets/Java/Script.js
@@ -1,9 +1,14 @@
+//constants
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 
+
+// keeps users from answering before things are loaded
+let acceptingAnswers = false;
+
+//variables
 let currentQuestion = {};
-let acceptingAnswers = true;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -42,7 +47,7 @@ const CORRECT_BONUS = 1;
 const MAX_QUESTIONS = 3;
 
 
-// start game function with score at 0
+// start game function with score at 0 calls to getNewQuestion to display questions/answers
 startGame = () => {
     questionCounter = 0;
     score = 0;
@@ -51,11 +56,42 @@ startGame = () => {
 };
 
 getNewQuestion = () => {
+    if(availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS){
+        return window.location.assign("/gameOver.HTML");
+    }
+    // displays question
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
     console.log
+
+    // displays answers
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+
+    });
+
+    //splices questions so they are not reused
+    availableQuestions.splice(questionIndex, 1);
+    
+    //allows answer to be selected
+    acceptingAnswers= true;
 }
+
+
+    //event listeners for each question
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        getNewQuestion();
+    })
+})
 
 startGame ();
